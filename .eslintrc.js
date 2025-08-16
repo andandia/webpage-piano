00eca0cd770c2030351c8cec4737f67f0880705b
e <a>
2025-08-16 19:19:55 +0900

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/airbnb'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+    'linebreak-style': ['error', 'windows'],
+  },
+};
